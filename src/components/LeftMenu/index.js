363491ebcd837512e95561d405a8e0d0226b2c85
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-11-05 16:11:40 -0300

    add left menu component

---
+++ src/components/LeftMenu/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+import Drawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import LabelIcon from '@mui/icons-material/Label';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+
+import SearchBar from '../Search/';
+import ListItemWithIcon from '../ListItemWithIcon';
+
+export default function LeftMenu({ ...props }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const commomItems = {
+    "Questões": <QuestionMarkIcon style={{ color: "#f0f0f0" }} />,
+    "Tags": <LabelIcon style={{ color: "#f0f0f0" }} />,
+    "Rankings": <MilitaryTechIcon style={{ color: "#f0f0f0" }} />
+  }
+
+  return (
+    <Drawer 
+      variant="permanent"
+      PaperProps={{
+        sx: {
+          backgroundColor: "#181818",
+          borderRight: "1px #F0F0F0 solid",
+          zIndex: "0"
+        }
+      }}
+    >
+      <Toolbar sx={{ "margin-bottom": "1em" }} />
+      <Box sx={{ padding: "1em" }}>
+        <SearchBar setSearchQuery={setSearchQuery} />
+      </Box>
+      <Divider sx={{ "border-color": "#f0f0f0" }} />
+      <Box>
+        <List>
+          {Object.keys(commomItems).map(item => (
+            <ListItemWithIcon 
+              text={item}
+              icon={commomItems[item]}
+            />
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+}
