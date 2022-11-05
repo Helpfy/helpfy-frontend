1b30b0999020ea372838f8982585e73c7e122288
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-11-05 16:10:23 -0300

    add header semantic html tag

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,45 +8,50 @@ import ButtonBG from '../ButtonBG';
 
 import HelpfyLogo from '../HelpfyLogo/'
 
-
-export default function BasePage({ pageName="" }) {
+export default function Header({ pageName="" }) {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar sx={{ height: "4em", background: "#393E41" }}>
-          <HelpfyLogo />
-          <Typography 
-            variant="h5" 
-            sx={{ 
-              flexGrow: 3, 
-              "margin-left": "0.5em", 
-              "font-weight": "bold"
-            }}
-          >
-            Helpfy
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
-              flexGrow: 10,
-            }}
-          >
-            {pageName}
-          </Typography>
-          <Box display="flex" justifyContent="flex-end" sx={{display: "flex", gap: "0.5em"}}>
-            <ButtonBG
-              text="Registrar"
-              color="#F0F0F0"
-              background="#217CCB"
-            />
-            <ButtonBG
-              text="Entrar"
-              color="#181818"
-              background="#F0F0F0"
-            />
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  )
+    <header>
+      <Box sx={{ flexGrow: 1, zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <AppBar position="fixed">
+          <Toolbar sx={{ height: "4em", background: "#393E41" }}>
+            <HelpfyLogo />
+            <Typography 
+              variant="h5" 
+              sx={{ 
+                flexGrow: 3, 
+                "margin-left": "0.5em", 
+                "font-weight": "bold"
+              }}
+            >
+              Helpfy
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                flexGrow: 10,
+              }}
+            >
+              {pageName}
+            </Typography>
+            <Box 
+              display="flex"
+              justifyContent="flex-end"
+              sx={{display: "flex", gap: "0.5em"}}
+            >
+              <ButtonBG
+                text="Registrar"
+                color="#F0F0F0"
+                background="#217CCB"
+              />
+              <ButtonBG
+                text="Entrar"
+                color="#181818"
+                background="#F0F0F0"
+              />
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </header>
+  );
 }
