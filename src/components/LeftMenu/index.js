0169dc1ea1354b3724ec4bce4ec73b5d687c1728
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-11-06 19:33:51 -0300

    [WIP] refactor base page

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -1,28 +1,91 @@
 import React, { useState } from 'react';
 
-import Drawer from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
-import LabelIcon from '@mui/icons-material/Label';
 import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+import LabelIcon from '@mui/icons-material/Label';
+import { styled } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
 
 import SearchBar from '../Search/';
 import ListItemWithIcon from '../ListItemWithIcon';
 
-export default function LeftMenu({ ...props }) {
+const drawerWidth = 350;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const createDrawer = (theme) => (
+  styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      boxSizing: 'border-box',
+      ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+      }),
+      ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+      }),
+    }))
+);
+
+const createDrawerHeader = (theme) => (
+  styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  }))
+);
+
+export default function LeftMenu({
+  open,
+  handleDrawerClose 
+}) {
   const [searchQuery, setSearchQuery] = useState("");
+  const theme = useTheme();
+  const DrawerHeader = createDrawerHeader(theme);
+  const Drawer = createDrawer(theme);
 
   const commomItems = {
     "Questões": <QuestionMarkIcon style={{ color: "#f0f0f0" }} />,
     "Tags": <LabelIcon style={{ color: "#f0f0f0" }} />,
     "Rankings": <MilitaryTechIcon style={{ color: "#f0f0f0" }} />
-  }
+  };
 
   return (
-    <Drawer 
+    <Drawer
       variant="permanent"
+      open={open}
       PaperProps={{
         sx: {
           backgroundColor: "#181818",
@@ -33,26 +96,32 @@ export default function LeftMenu({ ...props }) {
         }
       }}
     >
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose} sx={{ color: "#F0F0F0" }}>
+          {theme.direction === 'rtl' ? 
+            <ChevronRightIcon /> : 
+            <ChevronLeftIcon />}
+        </IconButton>
+      </DrawerHeader>
       <Box
         sx={{
           padding: "1em",
           display: "flex", 
-          "justify-content": "center" 
+          justifyContent: "center" 
         }}
       >
         <SearchBar setSearchQuery={setSearchQuery} />
       </Box>
-      <Divider sx={{ "border-color": "#f0f0f0" }} />
-      <Box>
-        <List>
-          {Object.keys(commomItems).map(item => (
-            <ListItemWithIcon 
-              text={item}
-              icon={commomItems[item]}
-            />
-          ))}
-        </List>
-      </Box>
+      <Divider sx={{ borderColor: "#f0f0f0" }} />
+      <List>
+        {Object.keys(commomItems).map(item => (
+          <ListItemWithIcon 
+            key={item}
+            text={item}
+            icon={commomItems[item]}
+          />
+        ))}
+      </List>
     </Drawer>
   );
 }
--- src/pages/BasePage/index.js
+++ src/pages/BasePage/index.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-import Header from '../../components/Header/';
-import LeftMenu from '../../components/LeftMenu/';
+import Header from '../../components/Header';
+import LeftMenu from '../../components/LeftMenu';
 
-import "./styles.css";
+const createDrawerHeader = () => (
+  styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  }))
+);
 
-export default function BasePage({ leftMenu = true, children }) {
+// TODO: colocar funcoes de criacao 
+//  em arquivos de util (adicionar importacoes se necessário)
+// TODO: Colocar constantes como drawerWidth em arquivo de constantes
+
+const drawerWidth = 350;
+export default function BasePage() {
+
+  const [open, setOpen] = useState(false);
+  const DrawerHeader = createDrawerHeader();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="layout">
-      <div className="header">
-        <Header />
-      </div>
-      {leftMenu && (
-        <div className="left-menu">
-          <LeftMenu />
-        </div>
-      )}
-      <div className="content">
-        {children}
-      </div>
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <Header
+        handleDrawerOpen={handleDrawerOpen}
+        open={open}
+        drawerWidth={drawerWidth}
+      />
+      <LeftMenu 
+        open={open} 
+        handleDrawerClose={handleDrawerClose}
+      />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Typography paragraph>
+        </Typography>
+        <Typography paragraph>
+        </Typography>
+      </Box>
+    </Box>
   );
 }
