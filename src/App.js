57af7659183925f7353d412e7a2bfe2d223d36a5
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-12-04 19:53:22 -0300

    add theme configuration and refactor router

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import Router from './Router.js';
+
+const theme = createTheme({
+  white: {
+    main: "#F0F0F0"
+  },
+});
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  );
+}
