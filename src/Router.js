da44b394fcfa2c853a28af9a3d12dd21e9511b8e
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-12-08 20:16:36 -0300

    add create question page

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import RulesPage from './pages/RulesPage';
-import AnswersListPage from './pages/AnswersListPage';
+import RulesPage from "./pages/RulesPage";
+import AnswersListPage from "./pages/AnswersListPage";
+import CreateQuestionPage from "./pages/CreateQuestionPage";
 
 export default function Router() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/rules' element={<RulesPage />}/>        
-          <Route exact path='/' element={<AnswersListPage />}/>        
-        </Routes>
-      </BrowserRouter>  
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/rules" element={<RulesPage />} />
+        <Route exact path="/" element={<AnswersListPage />} />
+        <Route exact path="/create-question" element={<CreateQuestionPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/CreateQuestionPage/index.js
@@ -0,0 +1,10 @@
+import BasePage from "../BasePage";
+import InputTags from "../../components/InputTags";
+
+export default function CreateQuestionPage() {
+  return (
+    <BasePage pageName="Create Question">
+      <InputTags />
+    </BasePage>
+  );
+}
