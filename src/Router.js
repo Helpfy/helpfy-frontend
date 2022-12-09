6fb737058bcf640f3b63f0626e1fd693266e5877
gabriel souto <gabriel@pop-os.localdomain>
2022-12-09 20:17:01 -0300

    finish login

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HOME, REGISTER, RULES } from "./constants";
+import { HOME, REGISTER, RULES, SIGN_IN } from "./constants";
 import RulesPage from "./pages/RulesPage";
 import AnswersListPage from "./pages/AnswersListPage";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 export default function Router() {
   return (
@@ -12,6 +13,7 @@ export default function Router() {
         <Route exact path={HOME.route} element={<AnswersListPage />} />
         <Route exact path={RULES.route} element={<RulesPage />} />
         <Route exact path={REGISTER.route} element={<RegisterPage />} />
+        <Route exact path={SIGN_IN.route} element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
