6e14debdf1c88adac22316add0a65ffae693f233
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-12-11 22:41:31 -0300

    add new routes

--- src/constants.js
+++ src/constants.js
@@ -6,4 +6,4 @@ export const SIGN_IN = { route: "/login", title: "Entrar" };
 export const REGISTER = { route: "/register", title: "Registrar" };
 export const NEW_ASK = { route: "/new-ask", title: "Faça Uma Pergunta" };
 export const RULES = { route: "/rules", title: "Regras Da Comunidade" };
-export const ASK = { route: "/ask/:id", title: "Questão Aberta" };
+export const ASK = { route: "/ask/:id", title: "Questão" };
