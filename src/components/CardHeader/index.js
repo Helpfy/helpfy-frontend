e2fbfbfd4288316e146743a3e0d21bb21cea0ee7
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-12-11 22:45:18 -0300

    add card header component

---
+++ src/components/CardHeader/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import CardOptions from '../../components/CardOptions';
+
+export default function CardHeader({
+  username,
+  userpicture,
+  time,
+  resumed=true
+}) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between"
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          gap: "1em",
+          "align-items": "center"
+        }}
+      >
+        <Avatar
+          alt={username}
+          src={userpicture}
+          sx={{ width: 50, height: 50 }}
+        />
+        <Typography
+          variant="subtitle1"
+          sx={{
+            "font-weight": "bold",
+            color: "#F0F0F0" 
+          }}
+        >
+          {username}
+          <Typography variant="subtitle2" sx={{ color: "#898C8E" }}>
+            {time}
+          </Typography>
+        </Typography>
+      </Box>
+      {!resumed && <CardOptions />}
+    </Box>
+  );
+}
