6dff702f62736f2cb1cacc174f25805cdfbb563f
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2022-12-13 22:27:38 -0300

    add path to list item icon component

--- src/components/ListItemWithIcon/index.js
+++ src/components/ListItemWithIcon/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
+import { Link } from "react-router-dom";
+
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 
-export default function ListItemWithIcon({ text, icon }) {
+export default function ListItemWithIcon({ text, icon, path }) {
   return (
-    <ListItem 
-      key={text}
-      disablePadding
-      sx={{ "& *": { color: "#f0f0f0" } }}
-    >
-      <ListItemButton>
-        <ListItemIcon>
-          {icon}
-        </ListItemIcon>
-        <ListItemText primary={text} />
-      </ListItemButton>
-    </ListItem>
+    <Link to={path}>
+      <ListItem
+        key={text}
+        disablePadding
+        sx={{ "& *": { color: "#f0f0f0" } }}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            {icon}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItemButton>
+      </ListItem>
+    </Link>
   );
 
 }
