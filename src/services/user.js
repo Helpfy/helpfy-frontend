1fabd11a9291f6c18f34ad25113f1935550c69ea
gabriel souto <gabriel@pop-os.localdomain>
2022-12-16 22:28:32 -0300

    finish register

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,27 @@
 import api from "./api";
 export const UserService = {
+  registerUser(body) {
+    return api.post(`/users`, body);
+  },
+
   login(body) {
     return api.post(`/login`, body);
+  },
+
+  update(user, token) {
+    return api.put(`/users/${encodeURI(user.id)}`, user, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+
+  uploadImage(file, token) {
+    return api.post(`/upload-image`, file, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    });
   },
 };
