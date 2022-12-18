935553a714d233e279065f23424ae8a655a9d86f
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2022-12-18 18:10:57 -0300

    refactor create questiosn page

---
+++ src/pages/CreateQuestionPage/form.js
@@ -0,0 +1,118 @@
+import {
+  Box,
+  Button,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import MDEditor from "@uiw/react-md-editor";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import rehypeSanitize from "rehype-sanitize";
+import InputTags from "../../components/InputTags";
+import { useSnackbar } from "notistack";
+import { QuestionService } from "../../services/question";
+import { AuthContext } from "../../context/AuthContext";
+
+export default function CreateQuestionForm() {
+  const [title, setTitle] = useState("");
+  const [questionText, setQuestionText] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const { user, token } = useContext(AuthContext);
+
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  const goToQuestionsPage = () => {
+    navigate("/ask");
+  };
+
+  const handleTagDelete = (tagToRemove) => {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  };
+
+  const handleTagAdd = (newTag) => {
+    if (newTag !== "" && !tags.includes(newTag)) {
+      setTags([...tags, newTag]);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const body = {
+      title: title,
+      tags: tags,
+      body: questionText,
+    };
+    setLoading(true);
+    QuestionService.createQuestion(user, body, token)
+      .then((response) => {
+        enqueueSnackbar("Questão criada com sucesso.", { variant: "success" });
+        goToQuestionsPage();
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        enqueueSnackbar("Aconteceu um erro.", { variant: "error" });
+        console.log(err);
+      });
+  };
+
+  return loading ? (
+    <CircularProgress />
+  ) : (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ display: "flex", justifyContent: "center" }}
+    >
+      <Stack sx={{ width: "80%" }} spacing={1}>
+        <InputTags
+          values={tags}
+          handleDelete={handleTagDelete}
+          handleAdd={handleTagAdd}
+        />
+        <FormControl>
+          <InputLabel htmlFor="title-component">Title</InputLabel>
+          <OutlinedInput
+            sx={{ background: "#393E41" }}
+            id="title-component"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            label="Tag"
+          />
+        </FormControl>
+        <MDEditor
+          value={questionText}
+          onChange={setQuestionText}
+          previewOptions={{
+            rehypePlugins: [[rehypeSanitize]],
+          }}
+          visibleDragbar={false}
+          highlightEnable={false}
+          height={250}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{
+            mt: 3,
+            mb: 2,
+            color: "#F0F0F0",
+            "&:hover": {
+              background: "#217CCB",
+              filter: "brightness(85%)",
+            },
+          }}
+        >
+          Enviar
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
